7468bfa6f69cd2712bbc39e8ef7f5a0253458dfa
Sayan Bera <sayandbera@gmail.com>
2025-07-06 21:43:48 +0530

    Implement Vapi AI

--- app/companions/[companionId]/page.tsx
+++ app/companions/[companionId]/page.tsx
@@ -1,7 +1,65 @@
+import CompanionComponent from "@/components/CompanionComponent";
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const CompanionSessionPage = () => {
-  return <div>CompanionSessionPage</div>;
+interface CompanionSessionPageProps {
+  params: Promise<{ companionId: string }>;
+}
+
+const CompanionSessionPage = async ({ params }: CompanionSessionPageProps) => {
+  const { companionId } = await params;
+  const companion = await getCompanion(companionId);
+  const user = await currentUser();
+
+  const { name, subject, title, topic, duration } = companion;
+
+  if (!user) {
+    redirect("/sign-in");
+  } else if (!name) {
+    redirect("/companions");
+  }
+
+  return (
+    <main>
+      <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+        <div className="flex items-center gap-2">
+          <div
+            className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt={subject}
+              width={35}
+              height={35}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-2xl">{name}</p>
+              <div className="subject-badge max-sm:hidden">{subject}</div>
+            </div>
+            <p className="text-lg">{topic}</p>
+          </div>
+        </div>
+        <div className="items-start text-2xl max-md:hidden">
+          {duration} minutes
+        </div>
+      </article>
+
+      <CompanionComponent
+        {...companion}
+        companionId={companionId}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
+    </main>
+  );
 };
 
 export default CompanionSessionPage;
